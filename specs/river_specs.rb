0e367bf35e503023716c4e574701229d7c22d420
SwayGatsby <kayla.fowler@hotmail.com>
2017-10-03 18:15:26 +0100

    initial commit - added river & can get name

---
+++ specs/river_specs.rb
@@ -0,0 +1,15 @@
+require_relative("../river.rb")
+require("minitest/autorun")
+require("minitest/rg")
+
+class TestRiver < MiniTest::Test
+
+  def setup
+    @river = River.new("Amazon")
+  end
+
+  def test_get_river_name
+    assert_equal("Amazon", @river.name())
+  end
+
+end
